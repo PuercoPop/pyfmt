fdf3a2c469ffa5e72d7841e5b690a42b9017d3bc
Javier Olaechea <pirata@gmail.com>
2013-07-06 03:23:04 -0400

    Rewrote to use mode properly the NodeVisitor's functionality
        See: http://greentreesnakes.readthedocs.org/en/latest/manipulating.html

--- example.py
+++ example.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 import ast
 from log import log
-from printers import dispatch
+from printers import PrinterVisitor
 
 
 if __name__ == "__main__":
     with open("samples/arithmetic.py", "r") as f:
         source_ast = ast.parse(f.read())
     with open("my_file.py", "w") as output:
-        dispatch(source_ast, output)
+        PrinterVisitor(output).visit(source_ast)
--- printers.py
+++ printers.py
@@ -3,90 +3,29 @@ import ast
 from log import log
 
 
-def dispatch(node, output):
-    if isinstance(node, ast.Module):
-        mv = ModuleVisitor()
-        mv.visit(node, output)
-    elif isinstance(node, ast.Expr):
-        ev = ExprVisitor()
-        ev.visit(node, output)
-    elif isinstance(node, ast.BinOp):
-        bov = BinOpVisitor()
-        bov.visit(node, output)
-    elif isinstance(node, ast.Num):
-        nv = NumVisitor()
-        nv.visit(node, output)
-    elif isinstance(node, ast.Add):
-        av = AddVisitor()
-        av.visit(node, output)
-    elif isinstance(node, ast.Sub):
-        sv = SubVisitor()
-        sv.visit(node, output)
-    elif isinstance(node, ast.Div):
-        dv = DivVisitor()
-        dv.visit(node, output)
-    elif isinstance(node, ast.Pow):
-        pv = PowVisitor()
-        pv.visit(node, output)
-    elif isinstance(node, ast.Mod):
-        mv = ModVisitor()
-        mv.visit(node, output)
-    else:
-        import ipdb; ipdb.set_trace()
+class PrinterVisitor(ast.NodeVisitor):
+    def __init__(self, output):
+        super(PrinterVisitor, self).__init__()
+        self.output = output
 
+    def visit_Add(self, node):
+        self.output.write(" + ")
 
-class BasePrinterVisitor(ast.NodeVisitor):
-    def visit(self, node):
-        log.info("In {node_name}".format(node_name=node.__class__.__name__))
+    def visit_Sub(self, node):
+        self.output.write(" - ")
 
+    def visit_Div(self, node):
+        self.output.write(" / ")
 
+    def visit_Pow(self, node):
+        self.output.write(" ** ")
 
-class ModuleVisitor(BasePrinterVisitor):
-    def visit(self, node, output):
-        super(ModuleVisitor, self).visit(node)
-        for node in node.body:
-            dispatch(node, output)
+    def visit_Mod(self, node):
+        self.output.write(" % ")
 
-class ExprVisitor(BasePrinterVisitor):
-    def visit(self, node, output):
-        super(ExprVisitor, self).visit(node)
-        dispatch(node.value, output)
-        output.write("\n")
+    def visit_Num(self, node):
+        self.output.write(str(node.n))
 
-class BinOpVisitor(BasePrinterVisitor):
-    def visit(self, node, output):
-        super(BinOpVisitor, self).visit(node)
-        dispatch(node.right, output)
-        dispatch(node.op, output)
-        dispatch(node.left, output)
-
-class NumVisitor(BasePrinterVisitor):
-    def visit(self, node, output):
-        super(NumVisitor, self).visit(node)
-        output.write(str(node.n))
-
-class AddVisitor(BasePrinterVisitor):
-    def visit(self, node, output):
-        super(AddVisitor, self).visit(node)
-        output.write(" + ")
-
-class SubVisitor(BasePrinterVisitor):
-    def visit(self, node, output):
-        super(SubVisitor, self).visit(node)
-        output.write(" - ")
-
-class DivVisitor(BasePrinterVisitor):
-    def visit(self, node, output):
-        super(DivVisitor, self).visit(node)
-        output.write(" / ")
-
-class PowVisitor(BasePrinterVisitor):
-    def visit(self, node, output):
-        super(PowVisitor, self).visit(node)
-        output.write(" ** ")
-
-
-class ModVisitor(BasePrinterVisitor):
-    def visit(self, node, output):
-        super(ModVisitor, self).visit(node)
-        output.write(" % ")
+    def visit_Expr(self, node):
+        self.generic_visit(node)
+        self.output.write("\n")
